6c4bb5326c448a094f4e93ab117fcebd92d4605b
Zach Postle <zpostle@gmail.com>
2013-09-18 19:32:48 -0400

    This is the update on the HTLM and CSS code for p1.5.chargemeupsidedown.com webpage

--- index.php
+++ index.php
@@ -1,1 +1,54 @@
-This is my first text for P1.
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+	<head>
+		<title>"Charge Me Upside Down!"...As said by Zach Postle"</title>
+		<!-- Some limited style applied to the webpage. -->
+		<style type="text/css">
+  			body {
+  				padding-left: 13em;
+    			color: purple;
+    			background-color: #d8da3a }
+    		ul.navbar {
+    			list-style-type: none;
+    			padding: 0;
+    			margin: 0;
+    			top: 4em;
+    			left: 3em;
+    			width: 12em;
+    			position: absolute;}
+  		</style>
+	</head>
+
+	<body>
+
+		<!-- This is menu that goes to my facebook and celticsblog.com. -->
+		<ul class="navbar">
+			<li><a href="http://www.celticsblog.com">My Celtics!</a>
+			<li><a href="https://www.facebook.com/zach.postle.98">My Facebook!</a>
+		</ul>
+		<h1>This may just be practice, but it's still me!</h1>
+			<p>So why am I here then??</p>
+			<p>To show you stuff like who my favorite athlete is:</p>
+			<!-- Main photo of the webpage -->
+			<img src='http://www.celticstown.com/wp-content/uploads/2012/08/
+			rajonrondo1.jpg'>
+		<h3>But what's your <b>NAME</b>??</h3>
+			<p>...Oh yes, of course, my name...I'm <b>Zach Postle</b></p>
+		<h2>Guess what...</h2>
+		<!-- Here's a brief little bio -->
+			<p>Today's actually my birthday believe it or not! I just turned 27.
+			   I went to school in Boston, and I'm originally from Upstate NY.
+			   This year, I got into programming. I started off learning object-
+			   oriented programming through Java and Python. Man, that had a steep
+			   learning curve. This eventually got me interested in web development
+			   specifically. I learned some HTML, CSS, and Javascript afterwards, 
+			   and now you see me here in your class. Thank god I learned how to
+			   have <b>perserverance</b> during my initial studies, because setting
+			   up this version control stuff was (<b>no exaggeration</b>) one of <b>
+			   THE</b> most confusing things I've ever attempted to learn.</p>
+
+		<h3>No worries though. I understand it now though. Otherwise, you wouldn't 
+			be seeing this wonderful little bio..</h3>  
+	</body>
+</html>
